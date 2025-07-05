94e3fac9005b471a5e430eb1fcd0504df0abece4
er1na <ern20501prg@gmail.com>
2025-07-05 23:15:12 +0900

    トップページの編集

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,16 @@
 import Header from './components/Header';
 import Calendar from './components/Calendar/Calendar';
+import QuickActions from './components/QuickActions/QuickActions';
+import RecentHistory from './components/RecentHistory/RecentHistory';
 
 export default function HomePage() {
   return (
     <main className="min-h-screen bg-blue-100 backdrop">
-      <div className="p-6 flex flex-col gap-6">
+      <div className="max-w-4xl mx-auto p-6 flex flex-col gap-6">
         <Header />
         <Calendar />
+        <QuickActions />
+        <RecentHistory />
       </div>
     </main>
   );
